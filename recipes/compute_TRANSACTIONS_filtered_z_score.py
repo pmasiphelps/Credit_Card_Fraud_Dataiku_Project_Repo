7e1924e75d3151334be0207b312dab50b95c532c
patrick.masi-phelps@dataiku.com <patrick.masi-phelps@dataiku.com>
2023-09-12 00:24:50 +0000

    Recipe CREDITCARDFRAUD.compute_TRANSACTIONS_filtered_z_score saved through notebook notebook editor for compute_TRANSACTIONS_filtered_z_score

--- recipes/compute_TRANSACTIONS_filtered_z_score.py
+++ recipes/compute_TRANSACTIONS_filtered_z_score.py
@@ -1,16 +1,21 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+from feature_engineering.column_transformations import filter_df_by_zscore
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 TRANSACTIONS_prepared = dataiku.Dataset("TRANSACTIONS_prepared")
 TRANSACTIONS_prepared_df = TRANSACTIONS_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+TRANSACTIONS_prepared_df = filter_df_by_zscore(TRANSACTIONS_prepared_df, "purchase_amount", zscore_threshold = 3)
 
-TRANSACTIONS_filtered_z_score_df = TRANSACTIONS_prepared_df # For this sample code, simply copy input to output
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 TRANSACTIONS_filtered_z_score = dataiku.Dataset("TRANSACTIONS_filtered_z_score")
-TRANSACTIONS_filtered_z_score.write_with_schema(TRANSACTIONS_filtered_z_score_df)
+TRANSACTIONS_filtered_z_score.write_with_schema(TRANSACTIONS_prepared_df)
